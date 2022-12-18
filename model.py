cb5033aba43b0a7f7605263ca5a2cd95a2b6279c
Evgeniy <zheinirk38@gmail.com>
2022-12-19 01:54:24 +0800

    added files

---
+++ model.py
@@ -0,0 +1,20 @@
+
+def get_cont_name(name):
+    return 0
+
+def get_cont_phone(phone):
+    return 0
+
+def get_cont_addres(addres):
+    return 0
+
+
+def put_cont(name, phone, addres):
+    return 0
+
+def import_directory(directory):
+    return 0
+
+def export_directory(directory):
+    return 0
+
