ef04644679b86708b8273dcd971f82a98f93b15e
Evgeniy <zheinirk38@gmail.com>
2022-12-19 16:31:25 +0800

    добавил в main вызовы методов из controller, протестировал, экспортировал справочник txt в 123_new.csv

---
+++ main.py
@@ -0,0 +1,8 @@
+import controller
+
+# directory = controller.import_dir()
+# print(directory)
+# controller.print_directory()
+# controller.find_contact()
+# controller.put_contact()
+controller.export()
